3ddf2e85a6c1cf42fddfe0fc0a5aac13fa67d8c5
GavIn Sokolof <sokolofg@gmail.com>
2019-03-22 15:43:48 -0400

    first real test

--- src/components/_tests_/App.test.js
+++ src/components/_tests_/App.test.js
@@ -5,6 +5,6 @@ import App from "../App";
 it("shows a comment box", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
-  console.log(div.innerHTML);
+  expect(div.innerHTML).toContain("Comment Box");
   ReactDOM.unmountComponentAtNode(div);
 });
