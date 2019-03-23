b9f53c71c524b8e1ec5587091d5e8e06118439a3
GavIn Sokolof <sokolofg@gmail.com>
2019-03-23 16:43:23 -0400

    comment box form

--- src/components/_tests_/App.test.js
+++ src/components/_tests_/App.test.js
@@ -1,14 +1,18 @@
 import React from "react";
-import CommentBox from "../CommentBox";
+import CommentBox from "components/CommentBox";
 import App from "../App";
 import { shallow } from "enzyme";
-import CommentList from "../CommentList";
+import CommentList from "components/CommentList";
 
+let component;
+
+beforeEach(() => {
+  component = shallow(<App />);
+});
 it("shows a comment box", () => {
-  const component = shallow(<App />);
-
   expect(component.find(CommentList).length).toEqual(1);
 });
+
 it("shows a comment list", () => {
   expect(component.find(CommentList).length).toEqual(1);
 });
