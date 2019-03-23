74642fbf90d9ac16870bc4d2065a47de43439d33
GavIn Sokolof <sokolofg@gmail.com>
2019-03-23 12:47:21 -0400

    testing for both commentbox and commentlist

--- src/components/_tests_/App.test.js
+++ src/components/_tests_/App.test.js
@@ -2,8 +2,10 @@ import React from "react";
 import CommentBox from "../CommentBox";
 import App from "../App";
 import { shallow } from "enzyme";
+import CommentList from "../CommentList";
 
 it("shows a comment box", () => {
   const component = shallow(<App />);
   expect(component.find(CommentBox).length).toEqual(1);
+  expect(component.find(CommentList).length).toEqual(1);
 });
