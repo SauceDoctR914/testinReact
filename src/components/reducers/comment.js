4b94549d885f007922f198e8260ac897fee167b0
GavIn Sokolof <sokolofg@gmail.com>
2019-03-25 17:35:28 -0400

    setup reducer and store

---
+++ src/components/reducers/comment.js
@@ -0,0 +1,12 @@
+state = {
+  comments: []
+};
+
+const commentsReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default commentsReducer;
