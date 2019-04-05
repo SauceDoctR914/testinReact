a5e77e3840b6b5f70a7adab856cc5850056f256d
GavIn Sokolof <sokolofg@gmail.com>
2019-04-04 22:06:20 -0400

    updated form with select for weight

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -9,11 +9,42 @@ class CreateExercise extends Component {
     complete: false
   };
 
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
     return (
       <div>
         <form>
           <input name="title" value={this.state.title} />
+          <select
+            multiple={true}
+            onChange={this.handleChange}
+            value={[
+              5,
+              10,
+              15,
+              20,
+              25,
+              30,
+              35,
+              40,
+              45,
+              50,
+              55,
+              60,
+              65,
+              70,
+              75,
+              80,
+              85,
+              90,
+              95,
+              100
+            ]}
+          />
+          <input type="submit" value="Submit" />
         </form>
       </div>
     );
