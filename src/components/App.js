2adcb36f368b9c58d4fa8a66bb4c69f10e81c446
GavIn Sokolof <sokolofg@gmail.com>
2019-04-06 22:26:18 -0400

    set up two routes in app

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,30 @@ import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 import { Route, Switch } from "react-router-dom";
 import { withRouter } from "react-router-dom";
+
 export default () => {
   return (
-    <React.Fragment>
+    <div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={routerProps => <HomePage {...routerProps} />}
+        />
+        <Route
+          exact
+          path="/workouts"
+          render={routerProps => <WorkoutsContainer {...routerProps} />}
+        />
+        <div>
+          <CommentBox />
+          <CommentList />
+        </div>
+      </Switch>
       <div>
         <CommentBox />
         <CommentList />
       </div>
-    </React.Fragment>
+    </div>
   );
 };
