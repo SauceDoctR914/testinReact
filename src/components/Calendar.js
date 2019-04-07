ff23db010729b7cd2fbc3fb7c13595c88b77c85f
GavIn Sokolof <sokolofg@gmail.com>
2019-04-06 23:33:45 -0400

    starting to really build calendar component

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
+import Calendar from "react-calendar/dist/entry.nostyle";
 class Calendar extends Component {
+  weekdayshortname = this.weekdayshort.map(day => {
+    return (
+      <th key={day} className="week-day">
+        {day}
+      </th>
+    );
+  });
   render() {
     return <div />;
   }
