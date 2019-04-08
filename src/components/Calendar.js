77924e6f13753d8468239c250b25fc114829dcde
GavIn Sokolof <sokolofg@gmail.com>
2019-04-07 21:41:23 -0400

    added datetime package and state for calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import Calendar from "react-calendar/dist/entry.nostyle";
+import Moment from "moment";
 class Calendar extends Component {
+  weekdayshort = moment.weekdaysShort();
+  state = {
+    dateObject: moment()
+  };
+
   weekdayshortname = this.weekdayshort.map(day => {
     return (
       <th key={day} className="week-day">
