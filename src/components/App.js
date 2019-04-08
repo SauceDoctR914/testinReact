d25ec8413e95acf8df42071441bffefbc2ac1d87
GavIn Sokolof <sokolofg@gmail.com>
2019-04-07 21:52:44 -0400

    added route swtiched homepage to login

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,13 @@ import { Route, Switch } from "react-router-dom";
 import { withRouter } from "react-router-dom";
 
 export default () => {
+  let logout = () => localStorage.removeItem("jwt");
   return (
     <div>
       <Switch>
         <Route
           exact
-          path="/"
+          path="/homepage"
           render={routerProps => <HomePage {...routerProps} />}
         />
         <Route
@@ -18,10 +19,11 @@ export default () => {
           path="/workouts"
           render={routerProps => <WorkoutsContainer {...routerProps} />}
         />
-        <div>
-          <CommentBox />
-          <CommentList />
-        </div>
+        <Route
+          exact
+          path="/"
+          render={routerProps => <Login {...routerProps} />}
+        />
       </Switch>
       <div>
         <CommentBox />
