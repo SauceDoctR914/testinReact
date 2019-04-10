df6401b07f68c14195d89988e3298e604b0a6c20
GavIn Sokolof <sokolofg@gmail.com>
2019-04-10 00:04:18 -0400

    second attempt at async for login complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,30 +10,37 @@ class Login extends Component {
 
   login = async obj => {
     let URL;
-    let response = await fetch(URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify({
-        email: this.state.email,
-        username: this.state.username,
-        password: this.state.password
-      })
-    })
-      let jsonRes = await response.json())
-      .then(user => {
-        if (user.error) {
-          this.setState({ errors: true });
-        } else {
-          localStorage.setItem("jwt", user.jwt);
-          if (user.jwt) {
-            console.log("user", user);
-          }
-        }
-      })
-      .catch(console.error);
+    try {
+      let response = await fetch(URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          email: this.state.email,
+          username: this.state.username,
+          password: this.state.password
+        })
+      });
+      let user = await response.json();
+      localStorage.setItem("jwt", user.jwt);
+    } catch (e) {
+      console.error(e);
+      this.setState({ errors: true });
+      throw new Error("Login Failed");
+    }
+    // let user = await data(user => {
+    //   if (user.error) {
+    //     this.setState({ errors: true });
+    //   } else {
+    //     localStorage.setItem("jwt", user.jwt);
+    //     if (user.jwt) {
+    //       console.log("user", user);
+    //     }
+    //   }
+    // })
+    // .catch(console.error);
   };
 
   render() {
